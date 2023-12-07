815635f9728404203eafff62b74a84de9b807320
seinecle <1244100+seinecle@users.noreply.github.com>
2023-12-07 17:19:39 +0100

    file management

--- docs/asciidoctor-reveal.js-master/lib/asciidoctor-revealjs.rb
+++
@@ -1,7 +0,0 @@
-if RUBY_ENGINE == 'opal'
-  require 'asciidoctor-revealjs/converter'
-else
-  require 'asciidoctor' unless defined? Asciidoctor::Converter
-  require_relative 'asciidoctor-revealjs/converter'
-  require_relative 'asciidoctor-revealjs/version'
-end
--- docs/asciidoctor-reveal.js-master/test/asciidoctor-revealjs/version_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Asciidoctor::RevealjsTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Asciidoctor::Revealjs::VERSION
-  end
-end
